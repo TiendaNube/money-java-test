f78a43b60a77194a8a6eea334e767111631c6176
zeta <jzelayeta@tiendanube.com>
2020-07-07 16:02:45 -0300

    scalatest

---
+++ src/test/scala/MoneySpec.scala
@@ -0,0 +1,12 @@
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AsyncWordSpecLike
+
+class MoneySpec extends AsyncWordSpecLike with Matchers{
+
+  "money" should {
+    "dummy" in {
+      1 shouldBe 1
+    }
+  }
+
+}
